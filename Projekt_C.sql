33323138e51badfd63a7698d21e0f20b1fe353fc
arhademon200 <85450617+arhademon200@users.noreply.github.com>
2023-06-06 15:11:25 +0200

    Update Projekt_C.sql

--- Projekt_C.sql
+++ Projekt_C.sql
@@ -10,3 +10,10 @@ AS
 SELECT p.id_pracownicy, p.imie, p.nazwisko, COUNT(p.id_pracownicy) AS "ilosc_napraw"
 FROM pracownicy p LEFT JOIN pracownicy_naprawy n ON p.id_pracownicy=n.id_pracownicy
 GROUP BY p.id_pracownicy, p.imie, p.nazwisko;
+
+CREATE VIEW pracownicy_kilentow as
+select k.imie, k.nazwisko, p.imie as "imie pracownika", p.nazwisko as "nazwisko pracownika" from klient k 
+join sprzet s on k.id_klient = s.id_klient
+join naprawa n on s.id_naprawa = n.id_naprawa
+join pracownicy_naprawy pn on n.id_naprawa = pn.id_naprawa
+join pracownicy p on pn.id_pracownicy = p.id_pracownicy
